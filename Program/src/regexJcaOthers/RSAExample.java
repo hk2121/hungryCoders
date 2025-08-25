7892a595c5e11f0e457466af701a6117a2e91f47
hk2121 <hk2121999@gmail.com>
2025-08-25 20:17:00 +0530

    25-08

--- Program/src/regexJcaOthers/RSAExample.java
+++ Program/src/regexJcaOthers/RSAExample.java
@@ -1,4 +1,37 @@
 package regexJcaOthers;
 
+import javax.crypto.Cipher;
+import java.security.*;
+import java.util.Base64;
+
 public class RSAExample {
+    public static void main(String[] args) {
+        try {
+            //Generate RSA key pair
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(2048);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
+
+            //Message to be encrypted
+            String message = "This is a secret message";
+
+            //Encrypt the message
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+            byte[] encryptBytes = cipher.doFinal(message.getBytes());
+            String encryptedMessage = Base64.getEncoder().encodeToString(encryptBytes);
+            System.out.println("Encrypted Message: " + encryptedMessage);
+
+            //Decrypt the message
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+            String decryptedMessage = new String(decryptedBytes);
+            System.out.println("Decrypted Message: " + decryptedMessage);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
