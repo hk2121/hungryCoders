105e4798601e189b5ff483f200de1e79d98fbb93
hk2121 <hk2121999@gmail.com>
2025-09-10 23:01:48 +0530

    10-09

--- Program/src/collectionsandgenerics/FailFastTest.java
+++ Program/src/collectionsandgenerics/FailFastTest.java
@@ -1,4 +1,16 @@
 package collectionsandgenerics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class FailFastTest {
+    public void failFast(){
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3));
+        Iterator<Integer> iterator = numbers.iterator();
+        while(iterator.hasNext()){
+            Integer number = iterator.next();
+            numbers.add(50); // This line will throw error
+        }
+    }
 }
---
+++ Program/src/collectionsandgenerics/FailSafeTest.java
@@ -0,0 +1,18 @@
+package collectionsandgenerics;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class FailSafeTest {
+    public void failSafe(){
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>(Arrays.asList("a","b","c"));
+        Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()){
+            if(iterator.next().equals("b")){
+                list.remove("b"); // This will not cause an exception
+            }
+        }
+        System.out.println(list);//["a","c"]
+    }
+}
