3c78c33b953f21b7aa33cdf91d64cd1051267360
hk2121 <hk2121999@gmail.com>
2025-09-15 15:36:34 +0530

    13-09

---
+++ Program/src/collectionsandgenerics/TreeSetExample.java
@@ -0,0 +1,26 @@
+package collectionsandgenerics;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        Set<String> treeSet = new TreeSet<>();
+
+        //Add elements to the TreeSet
+        treeSet.add("Apple");
+        treeSet.add("Banana");
+        treeSet.add("Cherry");
+
+        //Check if an element exists
+        System.out.println("Contains 'Apple' : "+ treeSet.contains("Apple")); //Contains 'Apple' : true
+        //Remove an element
+        treeSet.remove("Banana");
+
+        //Iterate over the TreeSet
+        System.out.println("TreeSet Elements");
+        for(String fruit: treeSet){
+            System.out.println(fruit);
+        }
+    }
+}
