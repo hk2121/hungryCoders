4e6d54f88a37eb2ee4185841e23277d5e217d678
hk2121 <hk2121999@gmail.com>
2025-10-09 19:18:16 +0530

    09-10

--- Program/src/patternQuestions/invertedHalfPyramidWithNumbers.java
+++ Program/src/patternQuestions/invertedHalfPyramidWithNumbers.java
@@ -10,6 +10,8 @@ public class invertedHalfPyramidWithNumbers {
 //            System.out.println();
 //        }
 
+        System.out.println("==========================");
+
         for(int i=1;i<=n;i++){
             for( int j =1;j<=n-i+1;j++){
                 System.out.print(j);
