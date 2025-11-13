73b4299e2f84b0d363fa7e22c7aab727a2f743cd
hk2121 <hk2121999@gmail.com>
2025-11-13 21:25:29 +0530

    13-11

---
+++ Program/src/IOandNIO/FileOutputStreamExample.java
@@ -0,0 +1,26 @@
+package IOandNIO;
+
+import java.io.IOException;
+
+public class FileOutputStreamExample {
+    public static void main(String[] args) {
+        FileOutputStreamExample fos = null;
+        try{
+            fos= new FileOutputStreamExample("Example.txt");
+            String data = "Hello, World!";
+            //Convert string to byte array
+            byte[] byteArray = data.getBytes();
+            fos.write(byteArray);
+        } catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(fos != null){
+                    fos.close();
+                }
+            } catch(IOException ez){
+                ez.printStackTrace();
+            }
+        }
+    }
+}
